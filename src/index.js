013a94dff9b5d3a97bd62fbb491a21c9b08e131f
ShanjinurIslam <spondoncsebuet@gmail.com>
2020-08-26 20:22:06 +0600

    mongodb initiated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+require('dotenv').config()
 const app = require('express')()
 const http = require('http')
 
 server = http.createServer(app)
 
+require('./database')
+
 const socket = require('./socket')
 socket(server)
+
+const apiRouter = require("./routers/api")
+
+app.use('/api',apiRouter)
 
 app.get('/', (req, res) => {
     res.send("Welcome to Chithi Messeging Application")
 })
 
-server.listen(process.env.PORT)
+const port = process.env.PORT || 3000;
+
+server.listen(port);
